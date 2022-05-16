2c4e0a780172b555a4ace5e600e472162220895f
Antonin Roullet <antonin.roullet@protonmail.com>
2022-05-16 17:49:51 +0200

    Added command line input for choice of algo.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <tuple>
+#include <functional>
+#include <iostream>
 #include "gui.hpp"
 #include "sorting.hpp"
 
@@ -15,7 +17,19 @@ int main() {
     SDL_RenderSetScale(rend, std::get<0>(ASPECT_RATIO), std::get<1>(ASPECT_RATIO));
 
     auto vec = generate_random_vector();
-    quick_sort(vec, rend, 0, VECTOR_SIZE-1);
+
+    std::function<void(std::vector<unsigned>&, SDL_Renderer*)> sort_function;
+
+    char ans;
+    std::cout << "Which sorting algorithm?\n";
+    std::cin >> ans;
+
+    if (tolower(ans) == 's')
+        sort_function = selection_sort;
+    else if (tolower(ans) == 'b')
+        sort_function = bubble_sort;
+
+    sort_function(vec, rend);
 
     SDL_DestroyRenderer(rend);
     SDL_Quit();
