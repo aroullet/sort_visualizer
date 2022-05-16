973b6c261228f729fff785c3de3bb61967c15d4e
Antonin Roullet <antonin.roullet@protonmail.com>
2022-05-16 18:16:02 +0200

    Used std::bind so that quick sort and its different signature can be still be used during selection.

--- main.cpp
+++ main.cpp
@@ -5,31 +5,35 @@
 #include "sorting.hpp"
 
 constexpr std::tuple<int, int> ASPECT_RATIO(16, 9);
-
 constexpr int WIDTH = VECTOR_SIZE * std::get<0>(ASPECT_RATIO);
 constexpr int HEIGHT = UPPER_BOUND * std::get<1>(ASPECT_RATIO);
 
 int main() {
+
+   auto vec = generate_random_vector();
 
     SDL_Window* win = nullptr;
     SDL_Renderer* rend = nullptr;
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &win, &rend);
     SDL_RenderSetScale(rend, std::get<0>(ASPECT_RATIO), std::get<1>(ASPECT_RATIO));
 
-    auto vec = generate_random_vector();
-
-    std::function<void(std::vector<unsigned>&, SDL_Renderer*)> sort_function;
+   typedef std::function<void(std::vector<unsigned>, SDL_Renderer*, unsigned, unsigned)> sort_func_t;
+    sort_func_t fn;
 
     char ans;
-    std::cout << "Which sorting algorithm?\n";
+    std::cout << "Which sorting algorithm?\nOptions: Bubble Sort ('b'), Quick Sort ('q'), Selection Sort('s')\n";
     std::cin >> ans;
 
-    if (tolower(ans) == 's')
-        sort_function = selection_sort;
-    else if (tolower(ans) == 'b')
-        sort_function = bubble_sort;
+    using std::placeholders::_1, std::placeholders::_2;
 
-    sort_function(vec, rend);
+    if (tolower(ans) == 's')
+        fn = std::bind(&selection_sort, vec, rend);
+    else if (tolower(ans) == 'b')
+        fn = std::bind(&bubble_sort, vec, rend);
+    else if (tolower(ans) == 'q')
+        fn = std::bind(&quick_sort, vec, rend, 0, VECTOR_SIZE-1);
+
+    fn(vec, rend, 0, VECTOR_SIZE-1);
 
     SDL_DestroyRenderer(rend);
     SDL_Quit();
