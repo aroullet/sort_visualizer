e2b51abbe9d2722421290b22f48f7d64dfad6669
Antonin Roullet <antonin.roullet@protonmail.com>
2022-05-16 17:16:43 +0200

    Changed colors, reversed direction of lines to reflect the ascending order of the sort.

--- src/gui.cpp
+++ src/gui.cpp
@@ -2,7 +2,7 @@
 
 void update_gui(const std::vector<unsigned>& vec, SDL_Renderer* rend, unsigned i, unsigned j) {
 
-    SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(rend, 255, 255, 255, 255); // white
     SDL_RenderClear(rend);
 
     draw_vector(vec, rend, i, j);
@@ -16,21 +16,21 @@ void draw_vector(const std::vector<unsigned>& vec, SDL_Renderer* rend, unsigned 
     rect.x = 0; rect.y = 0;
     for (int i = 0; i < vec.size(); ++i) {
         if (i == current)
-            SDL_SetRenderDrawColor(rend, 0, 0, 255, 255);
+            SDL_SetRenderDrawColor(rend, 0, 0, 255, 255); // blue
         else if (i == compared)
-            SDL_SetRenderDrawColor(rend, 0, 255, 0, 255);
+            SDL_SetRenderDrawColor(rend, 0, 255, 0, 255); // green
         else
-            SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
-        SDL_RenderDrawLine(rend, i, 100, i, vec[i]);
+            SDL_SetRenderDrawColor(rend, 0, 0, 0, 0); // black
+        SDL_RenderDrawLine(rend, i, 99, i, 99-vec[i]);
     }
 }
 
 void comparison_found(const std::vector<unsigned>& vec, SDL_Renderer* rend, unsigned i, unsigned j) {
 
-    SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(rend, 255, 0, 0, 255); // red
 
-    SDL_RenderDrawLine(rend, i, 100, i, vec[i]);
-    SDL_RenderDrawLine(rend, j, 100, j, vec[j]);
+    SDL_RenderDrawLine(rend, i, 99, i, vec[i]);
+    SDL_RenderDrawLine(rend, j, 99, j, vec[j]);
 
     SDL_RenderPresent(rend);
     SDL_Delay(200);
